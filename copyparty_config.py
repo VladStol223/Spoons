f105b0d2585917d1bd6f8cd46b33588320c86886
jason1595 <jasondarby01@gmail.com>
2025-09-29 18:46:32 -0400

    Update copyparty_config.py
    updated comments for readability

--- copyparty_config.py
+++ copyparty_config.py
@@ -1,7 +1,7 @@
 # --- Copyparty / sync settings (leave USER_FOLDER empty to disable) ---
-COPYPARTY_BASE_URL   = "https://jasondarby.com"
-COPYPARTY_USER_FOLDER = None          # e.g. "vlad" or "friend"; "" or None disables sync
-COPYPARTY_USERNAME    = ""              # if server uses a single password only, leave username empty
-COPYPARTY_PASSWORD    = "poopinmabutt"  # the password you provided
-COPYPARTY_DAV_PREFIX  = ""          # common WebDAV mount on copyparty; try "" if your server uses root
+COPYPARTY_BASE_URL   = "https://jasondarby.com" #base url for copyparty server
+COPYPARTY_USER_FOLDER = None            # e.g. "vlad" or "friend"; "" or None disables sync. Quotation marks are manditory
+COPYPARTY_USERNAME    = ""              # leave username empty as copyparty uses password only login
+COPYPARTY_PASSWORD    = "poopinmabutt"  # Your copyparty password
+COPYPARTY_DAV_PREFIX  = ""              # common WebDAV mount on copyparty; try "" if your server uses root
 COPYPARTY_TIMEOUT_S   = 10              # network timeout
