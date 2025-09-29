9541390f34affc07df932181837b17b6ba46caef
jason1595 <jasondarby01@gmail.com>
2025-09-29 11:54:25 -0400

    Update copyparty_config.py
    updated base url

--- copyparty_config.py
+++ copyparty_config.py
@@ -1,5 +1,5 @@
 # --- Copyparty / sync settings (leave USER_FOLDER empty to disable) ---
-COPYPARTY_BASE_URL   = "https://television-until-memorial-wisconsin.trycloudflare.com"
+COPYPARTY_BASE_URL   = "https://jasondarby.com"
 COPYPARTY_USER_FOLDER = None          # e.g. "vlad" or "friend"; "" or None disables sync
 COPYPARTY_USERNAME    = ""              # if server uses a single password only, leave username empty
 COPYPARTY_PASSWORD    = "poopinmabutt"  # the password you provided
